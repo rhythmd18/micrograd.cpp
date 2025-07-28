806306f0ef64da5c088e6560147850c15f907ca4
Rhythm Dutta <duttarhythm18@gmail.com>
2025-07-28 22:06:09 +0530

    Modify Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,28 +8,25 @@
 int main()
 {
     
-    //auto x1 = Scalar::create(2.0, {}, "", "x1");
-    //auto x2 = Scalar::create(0.0, {}, "", "x2");
-    //
-    //// Weights w1, w2
-    //auto w1 = Scalar::create(-3.0, {}, "", "w1");
-    //auto w2 = Scalar::create(1.0, {}, "", "w2");
-    //
-    //// Bias b
-    //auto b = Scalar::create(6.8813735870195432, {}, "", "b");
+    auto x1 = Scalar::create(2.0, {}, "", "x1");
+    auto x2 = Scalar::create(0.0, {}, "", "x2");
+    
+    // Weights w1, w2
+    auto w1 = Scalar::create(-3.0, {}, "", "w1");
+    auto w2 = Scalar::create(1.0, {}, "", "w2");
+    
+    // Bias b
+    auto b = Scalar::create(6.8813735870195432, {}, "", "b");
 
-    //auto x1w1 = x1 * w1; x1w1->label = "x1*w1";
-    //auto x2w2 = x2 * w2; x2w2->label = "x2*w2";
-    //auto x1w1x2w2 = x1w1 + x2w2; x1w1x2w2->label = "x1*w1 + x2*w2";
-    //auto n = x1w1x2w2 + b; n->label = "n";
-    //auto o = n->tanh(); o->label = "o";
+    auto x1w1 = x1 * w1; x1w1->label = "x1*w1";
+    auto x2w2 = x2 * w2; x2w2->label = "x2*w2";
+    auto x1w1x2w2 = x1w1 + x2w2; x1w1x2w2->label = "x1*w1 + x2*w2";
+    auto n = x1w1x2w2 + b; n->label = "n";
+    auto o = n->tanh(); o->label = "o";
 
-    auto a = Scalar::create(3.0, {}, "", "a");
-    auto b = a + a; b->label = "b";
+    o->backward();
 
-    b->backward();
-
-    draw_dot(b);
+    draw_dot(o);
 
     return 0;
 }
